7741a7be5d99c8b872895d8a38c1aa1b42b76866
Chinese-BruceWayne <1184249450@qq.com>
2018-09-30 13:38:58 +0800

    active models

--- ctfsite/settings.py
+++ ctfsite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
+    'question.apps.QuestionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
