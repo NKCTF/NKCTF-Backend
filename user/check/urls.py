f505c0871ef8836a5342524dc36ee4d0a5873cb4
Chinese-BruceWayne <1184249450@qq.com>
2018-10-04 09:38:03 +0800

    [Add] API for checking team_name valid.

--- user/check/urls.py
+++ user/check/urls.py
@@ -7,4 +7,5 @@ app_name = "user_check"
 urlpatterns = [
     path("password/", views.Password.as_view(), name="password"),
     path("username/", views.Username.as_view(), name="username"),
+    path("team_name/", views.TeamName.as_view(), name="team_name"),
 ]
--- user/check/views.py
+++ user/check/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
-from user.models import User
+from user.models import User, Team
 
 
 def JsonResponseZh(json_data):
@@ -97,5 +97,47 @@ class Username(View):
 
     def get(self, request):
         self.username = request.GET.get("username")
+        self.code = self.check()
+        return JsonResponseZh(self.get_ret_dict())
+
+
+class TeamName(View):
+    team_name = code = None
+    banned_team_name = ["root", "admin", "superuser"]
+    error_msg = {
+        "length_out_range": "战队应该在 6 到 18 个字符之内",
+        "team_exist": "战队名已存在",
+        "is_reserved": "请勿使用保留战队名注册",
+    }
+    result = {
+        "length_out_range": True,
+        "team_exist": True,
+        "is_reserved": True,
+    }
+
+    def get_ret_dict(self):
+        return {
+            0: {"code": 0, "msg": "战队名合法"},
+            1: {"code": 1, "msg": "战队名不合法",
+                "error": " & ".join([v for k, v in self.error_msg.items() if self.result[k]])},
+            10: {"code": 10, "msg": "检测到攻击"},
+        }[self.code]
+
+    def check(self):
+        self.result["length_out_range"] = len(self.team_name) not in range(6, 18)  # TODO: 检查用户名长度是否在 6-18 之间
+        try:
+            Team.objects.get(team_name=self.team_name)
+            self.result["team_exist"] = True
+        except Team.DoesNotExist:
+            self.result["team_exist"] = False
+        self.result["is_reserved"] = self.team_name in self.banned_team_name
+        return 0 if ([v for v in self.result.values() if v] == []) else 1
+
+    def post(self, request):
+        self.code = 10
+        return JsonResponseZh(self.get_ret_dict())
+
+    def get(self, request):
+        self.team_name = request.GET.get("team_name")
         self.code = self.check()
         return JsonResponseZh(self.get_ret_dict())
