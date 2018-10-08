d704cd5f594eff8e5248b1ccaf80ec0ab77fb59b
Chinese-BruceWayne <1184249450@qq.com>
2018-10-08 10:18:37 +0800

    [Fix] debug logout view

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,7 @@ app_name = "user"
 urlpatterns = [
     path("login/", views.Login.as_view(), name="login"),
     path("signup/", views.Signup.as_view(), name="signup"),
-    path("logout/", views.logout, name="logout"),
+    path("logout/", views.log_out, name="logout"),
     path("auth_in/", views.user_auth_in, name="auth_in"),
     path("auth_back/", views.AuthLogin.as_view(), name="auth_back"),
     path("check/", include("user.check.urls")),
